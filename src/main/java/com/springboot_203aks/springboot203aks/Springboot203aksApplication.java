3ccb6c39a2b0ea5dc2c91912f413dc4d74ef2caa
asarkar <asarkar@promnetwork.com>
2019-03-20 16:14:54 -0400

    Initial commit

---
+++ src/main/java/com/springboot_203aks/springboot203aks/Springboot203aksApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_203aks.springboot203aks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot203aksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot203aksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_203aks/springboot203aks/Springboot203aksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot_203aks.springboot203aks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot203aksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
